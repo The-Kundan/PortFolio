1dec7b56d63fdea3d370fb1545009d87f44cb53f
Kundan kumar <106131038+The-Kundan@users.noreply.github.com>
2024-08-17 17:19:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,3 +5,10 @@ var typed= new Typed(".text", {
     backDelay:1000,
     loop:true
 })
+const menuBtn = document.querySelector('.menu-btn');
+const navbar = document.querySelector('.navbar');
+
+menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle('active');
+    navbar.classList.toggle('active');
+});
